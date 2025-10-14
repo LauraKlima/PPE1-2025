2a4b93bbe6d6536e8212c1c4b9b75dc17a298a1e
Laura Klima <lauraklima8@gmail.com>
2025-10-14 22:33:31 +0200

    Exo 1 : ajout script Locations.sh

---
+++ Exercices/Location.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "lieux en 2016:"
+cat 2016/* | grep Location | wc -l
+echo "lieux en 2017:"
+cat 2017/* | grep Location | wc -l
+echo "lieux en 2018:"
+cat 2018/* | grep Location | wc -l
