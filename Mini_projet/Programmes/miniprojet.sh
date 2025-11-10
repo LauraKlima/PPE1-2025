9c96d9c9545289cb3806a44818e2b4728cc1f501
Laura Klima <lauraklima8@gmail.com>
2025-11-11 00:07:39 +0100

    changement du script afin de passer de TSV a HTML

--- Mini_projet/Programmes/miniprojet.sh
+++ Mini_projet/Programmes/miniprojet.sh
@@ -5,6 +5,16 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
+echo "<html>"
+echo "<head>"
+    echo "<meta charset="UTF-8" />"
+echo "</head>"
+
+echo "<body>"
+echo "<table>"
+
+echo "<tr><th>Numéro</th><th>URLs</th><th>Nombre de mots</th><th>Encodage</th><th>Requête</th></tr>"
+
 fichier="$1"
 numerotation=1
 
@@ -13,7 +23,11 @@ do
     http_code=$(curl -s -L -o /dev/null -w "%{http_code}" "$line")
     nombre_mots=$(curl -s "$line" | wc -w)
     encodage=$(lynx -dump -source "$line" | grep -i "charset=" | head -n 1 | cut -d'"' -f2)
-    echo -e "$numerotation\t$http_code\t$nombre_mots\t$encodage\t${line}";
+    echo "<tr><td>$numerotation</td><td>${line}</td><td>$nombre_mots</td><td>$encodage</td><td>$http_code</td></tr>"
     ((numerotation++))
     sleep 1
-done < "$fichier"; 
+done < "$fichier"
+
+echo "</table>"
+echo "</body>"
+echo "</html>"
